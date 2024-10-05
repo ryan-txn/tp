003af0ddfbacb030daaa0159cde014da4100b029
kennethSty <kenneth.styppa@web.de>
2024-10-05 15:56:43 +0800

    Add javadocs to ChatParser class

--- src/main/java/seedu/healthmate/ChatParser.java
+++ src/main/java/seedu/healthmate/ChatParser.java
@@ -2,6 +2,10 @@ package seedu.healthmate;
 
 import java.util.Scanner;
 
+/**
+ * Encapsulates the main logic of the application by parsing user input into objects
+ * and storing them respectively.
+ */
 public class ChatParser {
 
     public static final String CALORIE_SIGNALLER = "/c";
@@ -15,6 +19,10 @@ public class ChatParser {
         this.mealOptions = historyTracker.loadMealOptions();
     }
 
+    /**
+     * Reads in user input from the command line
+     * and initiates the parsing process steered by one-token and two-token-based user prompts.
+     */
     public void run() {
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
@@ -35,6 +43,10 @@ public class ChatParser {
         }
     }
 
+    /**
+     * Steers the activation of features offered to the user via two-token commands
+     * @param userInput String user input from the command line
+     */
     public void multiCommandParsing(String userInput) {
         String[] inputTokens = userInput.split(" ");
         String commandToken1 = inputTokens[0].strip();
