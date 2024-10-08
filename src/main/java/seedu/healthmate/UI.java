abfe21bca2561554577c75f7e1752931aa03f1a6
ryan-txn <tanryan2002@gmail.com>
2024-10-09 02:28:55 +0800

    Made Indentation public for usage in ChatParser askForHealthGoal()

--- src/main/java/seedu/healthmate/UI.java
+++ src/main/java/seedu/healthmate/UI.java
@@ -3,7 +3,7 @@ package seedu.healthmate;
 public class UI {
 
     private static final String SEPARATOR = "_________________________________________________________________________";
-    private static final String INDENTATION = "      ";
+    public static final String INDENTATION = "      ";
     private static final String LOGO =
               INDENTATION + " |\n"
             + INDENTATION + "     \\\\|//\n"
