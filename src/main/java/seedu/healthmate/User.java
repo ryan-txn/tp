0affbe398252de1ae2dc4b817222efce97532e35
ryan-txn <tanryan2002@gmail.com>
2024-10-09 02:34:25 +0800

    User class clean up
    Make existing variables and methods static, include healthgoal and idealcalories variables, correct toString() method previously not zero-based

--- src/main/java/seedu/healthmate/User.java
+++ src/main/java/seedu/healthmate/User.java
@@ -2,26 +2,29 @@ package seedu.healthmate;
 
 import java.util.ArrayList;
 
-class User {
-    private ArrayList<WeightEntry> weightEntries;
-    private ArrayList<HeightEntry> heightEntries;
+public class User {
+    private static ArrayList<WeightEntry> weightEntries;
+    private static ArrayList<HeightEntry> heightEntries;
+
+    private static HealthGoal healthGoal;
+    private static int idealCalories;
 
     public User() {
-        this.weightEntries = new ArrayList<>();
-        this.heightEntries = new ArrayList<>();
+        weightEntries = new ArrayList<>();
+        heightEntries = new ArrayList<>();
     }
 
-    public void addWeightEntry(double weight) {
+    public static void addWeightEntry(double weight) {
         weightEntries.add(new WeightEntry(weight));
     }
 
-    public void addHeightEntry(double height) {
+    public static void addHeightEntry(double height) {
         heightEntries.add(new HeightEntry(height));
     }
 
     @Override
     public String toString() {
-        return "User with" + this.weightEntries.get(this.weightEntries.size())
-            + " and " + this.heightEntries.get(this.heightEntries.size());
+        return "User with" + weightEntries.get(weightEntries.size() - 1)
+            + " and " + heightEntries.get(heightEntries.size() - 1);
     }
 }
