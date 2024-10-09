f9383e1352fa1d478b57d9bd6a68784e7d4a7b91
kennethSty <kenneth.styppa@web.de>
2024-10-09 15:11:59 +0800

    Clean up separator and intendation into LINE

--- src/main/java/seedu/healthmate/UI.java
+++ src/main/java/seedu/healthmate/UI.java
@@ -2,10 +2,11 @@ package seedu.healthmate;
 
 public class UI {
     
-    private static final String lineSeparator = System.lineSeparator();
+    private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String SEPARATOR = "_________________________________________________________________________";
     private static final String INTENDATION = "      ";
-    private static final String frameLine = INTENDATION + SEPARATOR + lineSeparator;
+    private static final String LINE = INTENDATION + SEPARATOR;
+    private static final String FRAME_LINE = LINE + LINE_SEPARATOR;
     private static final String LOGO =
               INTENDATION + " |\n"
             + INTENDATION + "     \\\\|//\n"
@@ -18,22 +19,22 @@ public class UI {
 
 
     public static void printReply(String input, String actionPerformed) {
-        System.out.println(INTENDATION + SEPARATOR);
+        System.out.println(LINE);
         System.out.println(INTENDATION + actionPerformed + input);
-        System.out.println(INTENDATION + SEPARATOR);
+        System.out.println(LINE);
     }
 
     public static void printGreeting() {
         System.out.println(INTENDATION + LOGO);
-        System.out.println(INTENDATION + SEPARATOR);
+        System.out.println(LINE);
         System.out.println(INTENDATION + "Welcome to HealthMate");
         System.out.println(INTENDATION + "Let's get healthy!");
-        System.out.println(INTENDATION + SEPARATOR);
+        System.out.println(LINE);
     }
 
     public static void printFarewell() {
         System.out.println(INTENDATION + "Stay healthy!");
-        System.out.println(INTENDATION + SEPARATOR);
+        System.out.println(LINE);
     }
 
     public static void printString(String input) {
@@ -41,7 +42,7 @@ public class UI {
     }
 
     public static void printSeparator() {
-        System.out.println(INTENDATION + SEPARATOR);
+        System.out.println(LINE);
     }
 
     public static void printMealOptions(MealList mealOptions) {
@@ -70,29 +71,29 @@ public class UI {
         printSeparator();
     }
     public static void printCommands() {
-        System.out.println(INTENDATION + SEPARATOR);
+        System.out.println(LINE);
         for (String command: Commands.getAllCommands()){
             System.out.println(INTENDATION + "-" + command);
         }
-        System.out.println(INTENDATION + SEPARATOR);
+        System.out.println(LINE);
 
     }
 
     public static String simulateReply(String input, String actionPerformed) {
-        String line1 = lineSeparator;
-        String line2 = INTENDATION + actionPerformed + input + lineSeparator;
-        return  line1 + line2 + frameLine;
+        String line1 = LINE_SEPARATOR;
+        String line2 = INTENDATION + actionPerformed + input + LINE_SEPARATOR;
+        return  line1 + line2 + FRAME_LINE;
     }
 
     public static String simulateFareWell() {
-        String line1 = INTENDATION + "Stay healthy!" + lineSeparator;;
-        return line1 + frameLine;
+        String line1 = INTENDATION + "Stay healthy!" + LINE_SEPARATOR;;
+        return line1 + FRAME_LINE;
     }
 
     public static String simulateInitOutput() {;
-        String line2 = INTENDATION + "Meal Entries Loaded Successfully!" + lineSeparator;
-        String line3 = INTENDATION + "Meal Options Loaded Successfully!" + lineSeparator;
-        return frameLine + line2 + line3 + frameLine + INTENDATION + SEPARATOR;
+        String line2 = INTENDATION + "Meal Entries Loaded Successfully!" + LINE_SEPARATOR;
+        String line3 = INTENDATION + "Meal Options Loaded Successfully!" + LINE_SEPARATOR;
+        return FRAME_LINE + line2 + line3 + FRAME_LINE + LINE;
 
     }
 
@@ -103,13 +104,12 @@ public class UI {
      * @return
      */
     public static String toMealOptionsString(MealList mealOptions, String newMealString) {
-        String seperatorLine = INTENDATION + SEPARATOR;
         String mealOptionsString = "";
         for (int i = 0; i < mealOptions.size(); i++) {
-            mealOptionsString += INTENDATION + (i + 1) + ": " + mealOptions.toMealString(i) + lineSeparator;
+            mealOptionsString += INTENDATION + (i + 1) + ": " + mealOptions.toMealString(i) + LINE_SEPARATOR;
         }
-        mealOptionsString += INTENDATION + (mealOptions.size() + 1) + ": " + newMealString + lineSeparator;
-        return seperatorLine + lineSeparator + mealOptionsString + seperatorLine + lineSeparator;
+        mealOptionsString += INTENDATION + (mealOptions.size() + 1) + ": " + newMealString + LINE_SEPARATOR;
+        return LINE + LINE_SEPARATOR + mealOptionsString + LINE + LINE_SEPARATOR;
     }
 
 }
