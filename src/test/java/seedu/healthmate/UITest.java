d63b6927a7afec3989ab047727cb53816b45942c
ryryry-3302 <rjleong2002@gmail.com>
2024-10-09 16:04:20 +0800

    Fix: remove duplicate import

--- src/test/java/seedu/healthmate/UITest.java
+++ src/test/java/seedu/healthmate/UITest.java
@@ -3,8 +3,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
