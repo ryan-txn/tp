a2596ec8ecbe53ffb2941f79b2fc61260a776c6f
jonas <jonasseet@gmail.com>
2024-10-16 13:29:08 +0800

    Add JUnit test for HistoryTracker

---
+++ src/test/java/seedu/healthmate/HistoryTrackerTest.java
@@ -0,0 +1,16 @@
+package seedu.healthmate;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.File;
+
+public class HistoryTrackerTest {
+    
+    @Test
+    public void testCreateDataDirectory() {
+        HistoryTracker historyTracker = new HistoryTracker();
+        File dataDirectory = new File("data");
+        assertEquals(true, dataDirectory.exists(), "Data directory should be created");
+        dataDirectory.delete();
+    }
+}
