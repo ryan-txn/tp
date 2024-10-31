6c46b36ef6a54eda495a0ed1449a25a7b739c164
kennethSty <kenneth.styppa@web.de>
2024-10-31 13:38:28 +0800

    SLAP HistoricCalorieProgressCommand in multiCommandParsing

---
+++ src/main/java/seedu/healthmate/command/CommandPair.java
@@ -0,0 +1,24 @@
+package seedu.healthmate.command;
+
+import seedu.healthmate.Pair;
+
+public class CommandPair extends Pair<String, String[]> {
+
+    public CommandPair(String twoTokenCommand, String[] additionalCommands){
+        super(twoTokenCommand, additionalCommands);
+    }
+
+    public String getMainCommand() {
+        return super.t();
+    }
+
+    public String getCommandByIndex(int index) {
+        return super.u()[index];
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
